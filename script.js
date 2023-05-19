5cc6fb56b3ac8e46dd9cb524d677ee3cee555307
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-19 17:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-function calculateMinCost() {
-  //your code here
-  let sum=0;
-	for(let i=0;i<arr.length;i++){
-		sum+=arr[i];
-	}
-  return sum;
 
-  
-  
-}  
+  function connectRopes(input) {
+  // Step 1: Parse the input string
+  const ropeLengths = input.split(',').map(Number);
+
+  // Step 2: Create a min-heap
+  const minHeap = new MinHeap();
+  for (let i = 0; i < ropeLengths.length; i++) {
+    minHeap.insert(ropeLengths[i]);
+  }
+
+  // Step 3: Initialize total cost
+  let totalCost = 0;
+
+  // Step 4: Connect ropes until only one remains
+  while (minHeap.size() > 1) {
+    // Step 4a: Extract two minimum ropes
+    const rope1 = minHeap.extractMin();
+    const rope2 = minHeap.extractMin();
+
+    // Step 4b: Calculate sum of lengths
+    const sum = rope1 + rope2;
+
+    // Step 4c: Update total cost
+    totalCost += sum;
+
+    // Step 4d: Insert sum back into min-heap
+    minHeap.insert(sum);
+  }
+
+  // Step 6: Add length of final rope to total cost
+  totalCost += minHeap.extractMin();
+
+  // Step 7: Print total cost
+  document.getElementById('result').innerText = totalCost;
+}
